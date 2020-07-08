c5c15876bdc834a98b825f3e63b579b5107c6472
Carolina Ferreira <carol.mf14@gmail.com>
2020-07-08 15:26:33 -0300

    added users CRUD

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 const express = require('express');
 
- const server = express();
- const users = ['Pam', 'Dwight', 'Jim'];
+const server = express();
+server.use(express.json());
 
- server.get('/users/:index', (req, res) => {
+const users = ['Pam', 'Dwight', 'Jim'];
+
+// List all users
+server.get('/users', (req, res) => {
+ return res.json(users);
+});
+
+// List one user according to index
+server.get('/users/:index', (req, res) => {
   const {index} = req.params;
 
   return res.json(users[index]);
- })
- 
- server.listen(3000);
+})
+
+// Create a new user
+server.post('/users', (req, res) => {
+  const {name} = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+// Edit user
+server.put('/users/:index', (req, res) => {
+  const {index} = req.params;
+  const {name} = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+// Delete user
+server.delete('/users/:index', (req, res)=>{
+  const {index}= req.params;
+
+  users.splice(index,1);
+
+  return res.send();
+
+});
+
+server.listen(3000);
